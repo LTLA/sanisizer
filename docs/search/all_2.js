5bcc0e3c5c410375e37be4307b2e535379399cdc
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-06-02 15:58:43 +0000

    Deploying to gh-pages from @ LTLA/sanisizer@25da77d717d38cf92b30a58057434f660c47bdeb 🚀

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['cast_0',['cast',['../namespacesanisizer.html#aed4423ffa6beced95cee29c155e494d2',1,'sanisizer']]],
   ['cast_2ehpp_1',['cast.hpp',['../cast_8hpp.html',1,'']]],
-  ['casts_2',['Safe casts',['../index.html#autotoc_md2',1,'']]],
+  ['casting_2',['Casting',['../index.html#autotoc_md2',1,'']]],
   ['cmake_20with_20tt_20fetchcontent_20tt_3',['CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;',['../index.html#autotoc_md6',1,'']]],
   ['cmake_20with_20tt_20find_5fpackage_20tt_4',['CMake with &lt;tt&gt;find_package()&lt;/tt&gt;',['../index.html#autotoc_md7',1,'']]],
   ['comparisons_2ehpp_5',['comparisons.hpp',['../comparisons_8hpp.html',1,'']]],
