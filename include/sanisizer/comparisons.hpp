320e007680d1e6b5501b5cd98437055093cd5c3b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-08-20 10:46:08 -0700

    Added max() and min() functions for different integer types.

--- include/sanisizer/comparisons.hpp
+++ include/sanisizer/comparisons.hpp
@@ -2,6 +2,7 @@
 #define SANISIZER_COMPARISONS_HPP
 
 #include <type_traits>
+#include <limits>
 
 /**
  * @file comparisons.hpp
@@ -78,6 +79,58 @@ constexpr bool is_greater_than(Left_ l, Right_ r) {
 template<typename Left_, typename Right_>
 constexpr bool is_less_than_or_equal(Left_ l, Right_ r) {
     return !is_greater_than(l, r);
+}
+
+/**
+ * @tparam First_ First integer type. 
+ * @tparam Second_ Second integer type.
+ *
+ * @param first First non-negative value. 
+ * @param second Second non-negative value. 
+ *
+ * @return The smaller of `first` and `second`, in the smaller integer type of `First_` and `Second_`.
+ */
+template<typename First_, typename Second_>
+constexpr auto min(First_ first, Second_ second) {
+    if constexpr(std::numeric_limits<First_>::max() > std::numeric_limits<Second_>::max()) {
+        if (is_greater_than(first, second)) {
+            return second;
+        } else {
+            return static_cast<Second_>(first);
+        }
+    } else {
+        if (is_greater_than(first, second)) {
+            return static_cast<First_>(second);
+        } else {
+            return first;
+        }
+    }
+}
+
+/**
+ * @tparam First_ First integer type. 
+ * @tparam Second_ Second integer type.
+ *
+ * @param first First non-negative value. 
+ * @param second Second non-negative value. 
+ *
+ * @return The larger of `first` and `second`, in the larger integer type of `First_` and `Second_`.
+ */
+template<typename First_, typename Second_>
+constexpr auto max(First_ first, Second_ second) {
+    if constexpr(std::numeric_limits<First_>::max() > std::numeric_limits<Second_>::max()) {
+        if (is_greater_than(first, second)) {
+            return first;
+        } else {
+            return static_cast<First_>(second);
+        }
+    } else {
+        if (is_greater_than(first, second)) {
+            return static_cast<Second_>(first);
+        } else {
+            return second;
+        }
+    }
 }
 
 }
--- tests/src/comparisons.cpp
+++ tests/src/comparisons.cpp
@@ -52,3 +52,47 @@ TEST(Comparisons, GreaterThanOrEqual) {
     EXPECT_TRUE(sanisizer::is_greater_than_or_equal(1, 0u));
     EXPECT_FALSE(sanisizer::is_greater_than_or_equal(0u, 1));
 }
+
+TEST(Comparisons, Min) {
+    auto x = sanisizer::min(static_cast<unsigned char>(255), 100);
+    static_assert(std::is_same<decltype(x), unsigned char>::value);
+    EXPECT_EQ(x, 100u);
+    static_assert(sanisizer::min(static_cast<unsigned char>(255), 100) == 100u); // works at compile-time.
+
+    auto x2 = sanisizer::min(static_cast<unsigned char>(255), 1000);
+    static_assert(std::is_same<decltype(x2), unsigned char>::value);
+    EXPECT_EQ(x2, 255u);
+    static_assert(sanisizer::min(static_cast<unsigned char>(255), 1000) == 255u);
+
+    auto x3 = sanisizer::min(500u, 1000);
+    static_assert(std::is_same<decltype(x3), int>::value);
+    EXPECT_EQ(x3, 500);
+    static_assert(sanisizer::min(500u, 1000) == 500);
+
+    auto x4 = sanisizer::min(10000u, 1000);
+    static_assert(std::is_same<decltype(x4), int>::value);
+    EXPECT_EQ(x4, 1000);
+    static_assert(sanisizer::min(10000u, 1000) == 1000);
+}
+
+TEST(Comparisons, Max) {
+    auto x = sanisizer::max(static_cast<unsigned char>(255), 100);
+    static_assert(std::is_same<decltype(x), int>::value);
+    EXPECT_EQ(x, 255);
+    static_assert(sanisizer::max(static_cast<unsigned char>(255), 100) == 255); // works at compile-time.
+
+    auto x2 = sanisizer::max(static_cast<unsigned char>(255), 1000);
+    static_assert(std::is_same<decltype(x2), int>::value);
+    EXPECT_EQ(x2, 1000);
+    static_assert(sanisizer::max(static_cast<unsigned char>(255), 1000) == 1000);
+
+    auto x3 = sanisizer::max(500u, 1000);
+    static_assert(std::is_same<decltype(x3), unsigned int>::value);
+    EXPECT_EQ(x3, 1000u);
+    static_assert(sanisizer::max(500u, 1000) == 1000u);
+
+    auto x4 = sanisizer::max(10000u, 1000);
+    static_assert(std::is_same<decltype(x4), unsigned int>::value);
+    EXPECT_EQ(x4, 10000u);
+    static_assert(sanisizer::max(10000u, 1000) == 10000u);
+}
