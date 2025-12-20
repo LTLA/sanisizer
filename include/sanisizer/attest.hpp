dfc270e43af564187618fd903995272736cfbc03
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-12-20 18:28:12 +1100

    Renamed the functions for consistency, clarity.
    Added docstrings.

--- include/sanisizer/attest.hpp
+++ include/sanisizer/attest.hpp
@@ -5,16 +5,41 @@
 #include <type_traits>
 #include <cassert>
 
+/**
+ * @file attest.hpp
+ * @brief Create compile-time attestations.
+ */
+
 namespace sanisizer {
 
-template<typename Type_, bool is_unsigned_, Type_ limit_>
+/**
+ * @brief Attest to additional compile-time properties of an integer.
+ *
+ * @tparam Type_ Type of the integer.
+ * @tparam gez_ Whether the integer is greater than or equal to zero, known at compile time.
+ * This should be `true` if `Type_` is unsigned.
+ * @tparam max_ Maximum value of the integer, known at compile time.
+ * This should be non-negative.
+ *
+ * `gez_` and `max_` are compile-time attestations to the properties of the integer.
+ * This allows developers to specify more constraints on the integer than would otherwise be provided by `Type_`.
+ * For example, we could attest that an `int` can only store a non-negative value if `gez_ = true`,
+ * or that a `std::size_t` has a upper limit of `max_ = 2^31 - 1`.
+ */
+template<typename Type_, bool gez_, Type_ max_>
 struct Attestation {
     static_assert(std::is_integral<Type_>::value);
-    static_assert(!(std::is_unsigned<Type_>::value && !is_unsigned_));
+    static_assert(!(std::is_unsigned<Type_>::value && !gez_));
+    static_assert(max_ >= 0);
 
+    /**
+     * @value Value of the integer.
+     * This should be no greater than `max_`.
+     * If `gez_ = true`, this should be non-negative.
+     */
     constexpr Attestation(Type_ value) : value(value) {
-        assert(value <= limit_);
-        if constexpr(!std::is_unsigned<Type_>::value && is_unsigned_) {
+        assert(value <= max_);
+        if constexpr(!std::is_unsigned<Type_>::value && gez_) {
             assert(value >= 0);
         }
     }
@@ -30,35 +55,65 @@ struct Attestation {
      * @endcond
      */
 
+    /**
+     * Type of the integer.
+     */
     typedef Type_ Type;
 
-    static constexpr bool is_unsigned = is_unsigned_;
+    /**
+     * Whether the integer is greater than or equal to zero, known at compile time.
+     * This will be `true` if `Type_` is unsigned.
+     */
+    static constexpr bool gez = gez_;
 
-    static constexpr Type limit = limit_;
+    /**
+     * Maximum value of the integer, known at compile time.
+     * This will be non-negative.
+     */
+    static constexpr Type max = max_;
 
+    /**
+     * Value of the integer.
+     */
     Type_ value;
 };
 
+/**
+ * @tparam Value_ Integer or `Attestation`.
+ * @param x Integer value or an `Attestation` about an integer.
+ * @return `x` if it is already an integer, otherwise `Attestation::value`.
+ */
 template<typename Value_>
-constexpr auto get_value(Value_ val) {
+constexpr auto get_value(Value_ x) {
     if constexpr(std::is_integral<Value_>::value) {
-        return val;
+        return x;
     } else {
-        return val.value;
+        return x.value;
     }
 }
 
+/**
+ * @tparam Value_ Integer or `Attestation`.
+ * @param x Integer value or an `Attestation` about an integer.
+ * @return Whether the value of `x` is known to be greater than or equal to zero at compile time.
+ */
 template<typename Value_>
-constexpr bool is_unsigned(const Value_&) {
+constexpr bool get_gez(const Value_&) {
     if constexpr(std::is_integral<Value_>::value) {
         return std::is_unsigned<Value_>::value;
     } else {
-        return Value_::is_unsigned;
+        return Value_::gez;
     }
 }
 
+/**
+ * @tparam Value_ Integer or `Attestation`.
+ * @param x Integer value or an `Attestation` about an integer.
+ * @return `x` if it is already known to be greater than or equal to zero at compile time;
+ * otherwise, an `Attestation` that attests to this constraint.
+ */
 template<typename Value_>
-constexpr auto attest_unsigned(Value_ val) {
+constexpr auto attest_gez(Value_ val) {
     if constexpr(std::is_integral<Value_>::value) {
         if constexpr(std::is_unsigned<Value_>::value) {
             return val;
@@ -66,47 +121,67 @@ constexpr auto attest_unsigned(Value_ val) {
             return Attestation<Value_, true, std::numeric_limits<Value_>::max()>(val);
         }
     } else {
-        if constexpr(Value_::is_unsigned) {
+        if constexpr(Value_::gez) {
             return val;
         } else {
-            return Attestation<typename Value_::Type, true, Value_::limit>(val.value);
+            return Attestation<typename Value_::Type, true, Value_::max>(val.value);
         }
     }
 }
 
+/**
+ * @tparam Value_ Integer or `Attestation`.
+ * @param x Integer value or an `Attestation` about an integer.
+ * @return The maximum value of an integer that is known at compile time.
+ */
 template<typename Value_>
-constexpr auto get_limit(const Value_&) {
+constexpr auto get_max(const Value_&) {
     if constexpr(std::is_integral<Value_>::value) {
         return std::numeric_limits<Value_>::max();
     } else {
-        return Value_::limit;
+        return Value_::max;
     }
 }
 
-template<typename Limit_, Limit_ new_limit_, typename Value_>
-constexpr auto attest_limit(Value_ val) {
-    constexpr auto unsigned_new_limit = static_cast<typename std::make_unsigned<Limit_>::type>(new_limit_);
+/**
+ * @tparam Max_ Integer type of the new compile-time maximum.
+ * @tparam new_max_ The new compile-time maximum.
+ * @tparam Value_ Integer or `Attestation`.
+ * @param x Integer value or an `Attestation` about an integer.
+ * @return `x` if it is already known to be less than or equal to `new_max_`, 
+ * otherwise, an `Attestation` that attests to this constraint.
+ */
+template<typename Max_, Max_ new_max_, typename Value_>
+constexpr auto attest_max(Value_ val) {
+    constexpr auto unsigned_new_limit = static_cast<typename std::make_unsigned<Max_>::type>(new_max_);
     if constexpr(std::is_integral<Value_>::value) {
         constexpr auto max_value = static_cast<typename std::make_unsigned<Value_>::type>(std::numeric_limits<Value_>::max());
         if constexpr(max_value <= unsigned_new_limit) {
             return val; 
         } else {
-            return Attestation<Value_, std::is_unsigned<Value_>::value, static_cast<Value_>(new_limit_)>(val);
+            return Attestation<Value_, std::is_unsigned<Value_>::value, static_cast<Value_>(new_max_)>(val);
         }
     } else {
         typedef typename Value_::Type WrappedType;
-        constexpr auto max_value = static_cast<typename std::make_unsigned<WrappedType>::type>(Value_::limit);
+        constexpr auto max_value = static_cast<typename std::make_unsigned<WrappedType>::type>(Value_::max);
         if constexpr(max_value <= unsigned_new_limit) {
             return val;
         } else {
-            return Attestation<WrappedType, Value_::is_unsigned, static_cast<WrappedType>(new_limit_)>(val.value);
+            return Attestation<WrappedType, Value_::gez, static_cast<WrappedType>(new_max_)>(val.value);
         }
     }
 }
 
-template<typename Limit_, typename Value_>
-constexpr auto attest_limit_by_type(Value_ val) {
-    return attest_limit<Limit_, std::numeric_limits<Limit_>::max()>(val);
+/**
+ * @tparam Max_ Integer type of the new compile-time maximum.
+ * @tparam Value_ Integer or `Attestation`.
+ * @param x Integer value or an `Attestation` about an integer.
+ * @return `x` if it is already known to be less than or equal to the maximum value of `Max_`;
+ * otherwise, an `Attestation` that attests to this constraint.
+ */
+template<typename Max_, typename Value_>
+constexpr auto attest_max_by_type(Value_ val) {
+    return attest_max<Max_, std::numeric_limits<Max_>::max()>(val);
 }
 
 }
--- tests/src/attest.cpp
+++ tests/src/attest.cpp
@@ -9,24 +9,24 @@ TEST(Attest, Class) {
         constexpr sanisizer::Attestation<int, true, 20> val(5);
         static_assert(std::is_same<int, typename decltype(val)::Type>::value);
         static_assert(val.value == 5);
-        static_assert(decltype(val)::limit == 20);
-        static_assert(decltype(val)::is_unsigned);
+        static_assert(decltype(val)::max == 20);
+        static_assert(decltype(val)::gez);
     }
 
     {
         constexpr sanisizer::Attestation<unsigned, true, 100> val(10u);
         static_assert(std::is_same<unsigned, typename decltype(val)::Type>::value);
         static_assert(val.value == 10);
-        static_assert(decltype(val)::limit == 100);
-        static_assert(decltype(val)::is_unsigned);
+        static_assert(decltype(val)::max == 100);
+        static_assert(decltype(val)::gez);
     }
 
     {
         constexpr sanisizer::Attestation<long, false, 1000> val(20l);
         static_assert(std::is_same<long, typename decltype(val)::Type>::value);
         static_assert(val.value == 20);
-        static_assert(decltype(val)::limit == 1000);
-        static_assert(!decltype(val)::is_unsigned);
+        static_assert(decltype(val)::max == 1000);
+        static_assert(!decltype(val)::gez);
     }
 }
 
@@ -40,78 +40,78 @@ TEST(Attest, GetValue) {
 
 TEST(Attest, Unsigned) {
     {
-        static_assert(sanisizer::is_unsigned(sanisizer::Attestation<unsigned, true, 20>(5u)));
-        static_assert(!sanisizer::is_unsigned(sanisizer::Attestation<int, false, 20>(10)));
-        static_assert(sanisizer::is_unsigned(sanisizer::Attestation<int, true, 20>(5)));
-        static_assert(!sanisizer::is_unsigned(5));
-        static_assert(sanisizer::is_unsigned(5u));
+        static_assert(sanisizer::get_gez(sanisizer::Attestation<unsigned, true, 20>(5u)));
+        static_assert(!sanisizer::get_gez(sanisizer::Attestation<int, false, 20>(10)));
+        static_assert(sanisizer::get_gez(sanisizer::Attestation<int, true, 20>(5)));
+        static_assert(!sanisizer::get_gez(5));
+        static_assert(sanisizer::get_gez(5u));
     }
 
     {
-        constexpr auto thing = sanisizer::attest_unsigned(20u);
+        constexpr auto thing = sanisizer::attest_gez(20u);
         static_assert(std::is_same<decltype(thing), const unsigned>::value);
     }
 
     {
-        constexpr auto thing = sanisizer::attest_unsigned(20);
+        constexpr auto thing = sanisizer::attest_gez(20);
         static_assert(std::is_same<typename decltype(thing)::Type, int>::value);
         static_assert(thing.value == 20);
-        static_assert(decltype(thing)::limit > 20);
-        static_assert(decltype(thing)::is_unsigned);
+        static_assert(decltype(thing)::max > 20);
+        static_assert(decltype(thing)::gez);
     }
 
     {
-        constexpr auto thing = sanisizer::attest_unsigned(sanisizer::Attestation<unsigned, true, 20>(10u));
+        constexpr auto thing = sanisizer::attest_gez(sanisizer::Attestation<unsigned, true, 20>(10u));
         static_assert(std::is_same<typename decltype(thing)::Type, unsigned>::value);
         static_assert(thing.value == 10);
-        static_assert(decltype(thing)::limit == 20);
-        static_assert(decltype(thing)::is_unsigned);
+        static_assert(decltype(thing)::max == 20);
+        static_assert(decltype(thing)::gez);
     }
 
     {
-        constexpr auto thing = sanisizer::attest_unsigned(sanisizer::Attestation<int, false, 20>(10));
+        constexpr auto thing = sanisizer::attest_gez(sanisizer::Attestation<int, false, 20>(10));
         static_assert(std::is_same<typename decltype(thing)::Type, int>::value);
         static_assert(thing.value == 10);
-        static_assert(decltype(thing)::limit == 20);
-        static_assert(decltype(thing)::is_unsigned);
+        static_assert(decltype(thing)::max == 20);
+        static_assert(decltype(thing)::gez);
     }
 }
 
 TEST(Attest, Limit) {
     {
-        static_assert(5 == sanisizer::get_limit(sanisizer::Attestation<unsigned, true, 5>(5u)));
-        static_assert(10 == sanisizer::get_limit(sanisizer::Attestation<int, false, 10>(5)));
-        static_assert(20 == sanisizer::get_limit(sanisizer::Attestation<int, true, 20>(5)));
-        static_assert(sanisizer::get_limit(5) == std::numeric_limits<int>::max());
-        static_assert(sanisizer::get_limit(5u) == std::numeric_limits<unsigned>::max());
+        static_assert(5 == sanisizer::get_max(sanisizer::Attestation<unsigned, true, 5>(5u)));
+        static_assert(10 == sanisizer::get_max(sanisizer::Attestation<int, false, 10>(5)));
+        static_assert(20 == sanisizer::get_max(sanisizer::Attestation<int, true, 20>(5)));
+        static_assert(sanisizer::get_max(5) == std::numeric_limits<int>::max());
+        static_assert(sanisizer::get_max(5u) == std::numeric_limits<unsigned>::max());
     }
 
     {
-        constexpr auto thing = sanisizer::attest_limit_by_type<char>(20u);
+        constexpr auto thing = sanisizer::attest_max_by_type<char>(20u);
         static_assert(std::is_same<typename decltype(thing)::Type, unsigned>::value);
         static_assert(thing.value == 20);
-        static_assert(sanisizer::get_limit(thing) == std::numeric_limits<char>::max());
-        static_assert(decltype(thing)::is_unsigned);
+        static_assert(sanisizer::get_max(thing) == std::numeric_limits<char>::max());
+        static_assert(decltype(thing)::gez);
     }
 
     {
-        constexpr auto thing = sanisizer::attest_limit_by_type<unsigned>(20);
+        constexpr auto thing = sanisizer::attest_max_by_type<unsigned>(20);
         static_assert(std::is_same<decltype(thing), const int>::value);
         static_assert(thing == 20);
     }
 
     {
-        constexpr auto thing = sanisizer::attest_limit_by_type<char>(sanisizer::Attestation<int, true, 20>(10));
+        constexpr auto thing = sanisizer::attest_max_by_type<char>(sanisizer::Attestation<int, true, 20>(10));
         static_assert(thing.value == 10);
-        static_assert(decltype(thing)::limit == 20);
-        static_assert(decltype(thing)::is_unsigned);
+        static_assert(decltype(thing)::max == 20);
+        static_assert(decltype(thing)::gez);
     }
 
     {
-        constexpr auto thing = sanisizer::attest_limit_by_type<unsigned char>(sanisizer::Attestation<long, false, 2000>(10l));
+        constexpr auto thing = sanisizer::attest_max_by_type<unsigned char>(sanisizer::Attestation<long, false, 2000>(10l));
         static_assert(thing.value == 10);
-        static_assert(decltype(thing)::limit == std::numeric_limits<unsigned char>::max());
-        static_assert(!decltype(thing)::is_unsigned);
+        static_assert(decltype(thing)::max == std::numeric_limits<unsigned char>::max());
+        static_assert(!decltype(thing)::gez);
     }
 }
 
